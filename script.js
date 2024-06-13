dd3c4bb1895dc01640392de3f705058bcca0df2b
Souvik Paul <souvikpaul531@gmail.com>
2024-06-13 18:51:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.wrapper');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+
+
+registerLink.addEventListener('click',()=>{
+  wrapper.classList.add('active');
+});
+
+
+loginLink.addEventListener('click',()=>{
+  wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',()=>{
+  wrapper.classList.add('active-popup');
+});
